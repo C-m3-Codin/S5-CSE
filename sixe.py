03cf4152e8347fe7407291febc05b5d3a66c9418
cp <cyril199897@gmail.com>
2020-08-25 03:25:20 +0530

    added finding addressing modes

--- sixe.py
+++ sixe.py
@@ -25,7 +25,26 @@ for i in range(16):
 # print(bin_dict)
 # def c
 
-
+def FindAddress(flgs):
+    print("flags ",flgs)
+    if(flgs[3]=='1' and flgs[4]=='1'):
+        return "Base "
+    elif(flgs[3]=='0' and flgs[4]=='1'):
+        # print("Program Conter Relative")
+        return "Program Conter Relative "
+    elif(flgs[2]=='1' and flgs[3]=='0' and flgs[4]=='1' ):
+        return "Base relative with indexing "
+    elif(flgs[2]=='0' and flgs[3]=='0' and flgs[4]=='1'):
+        return "Program conter relative addressing"
+    elif(flgs[2]=='0' and flgs[3]=='0' and flgs[4]=='0'):
+        return "Direct "
+    elif(flgs[0]=='0' and flags[1]=='1'):
+        return "Immediate addressing "
+    elif(flgs[0]=='1' and flgs[1]=='0'):
+        return "Indirect "
+    else:
+        print(flgs[3],flgs[4])
+        return "not learned"
 
 def conToHEx(hexNum):
     bin_str=""
@@ -63,13 +82,16 @@ elif(l==2):
     reg1=binary[7:7+4]
     reg2=binary[7+4:7+4+4]
     print("\t op:\t",op,"\n\t reg 1 :\t",reg1,"\n\t reg 2 :\t",reg2)
+    # print("addressing mo")
 elif(l==3):
-    op=binary[0:7]
-    flags=binary[7:6+7]
-    disp=binary[6+7:len(binary)]
-    print("\t op :\t\t",op,"\n\t flags :\t",flags,"\n\t displacement :\t",disp)
+    op=binary[0:6]
+    flags=binary[6:6+6]
+    disp=binary[6+6:len(binary)]
+    addressing=FindAddress(flags)
+    print("\t op :\t\t",op,"\n\t flags :\t",flags,"\n\t displacement :\t",disp,"\n\t addressing Mode :\t",addressing)
 elif(l==4):
     op=binary[0:7]
     flags=binary[7:6+7]
     disp=binary[6+7:len(binary)]
-    print("\t op: \t",op,"\n\t flags:\t",flags,"\n\t address :\t",disp)
+    addressing=FindAddress(flags)
+    print("\t op: \t",op,"\n\t flags:\t",flags,"\n\t address :\t",disp,"\n\t addressing Mode :\t",addressing)
